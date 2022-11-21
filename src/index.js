076c5127ee4040a15a16b72e279bae1606e9fd04
Kenneth Elumba <kennethelumba@gmail.com>
2022-11-21 15:26:34 +0100

    add key to li in Game render method to implement time travel

--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ import './index.css';
           'Go to move #' + move :
           'Go to game start';
         return (
-          <li>
+          <li key={move}>
             <button onClick={() => this.jumpTo(move)}>{desc}</button>
           </li>
         )
